558bf7fb888762ab4b0b79712d02eb2069b66bc2
timof <timof@DESKTOP-G3P76R5>
2024-04-27 13:16:51 +0300

    Добавьте файлы проекта.

---
+++ СР 4 модуль.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+vector<int> function(vector<int>vec, int n) {
+	for (auto i = vec.begin(); i != vec.end(); ++i) {
+		*i += n;
+	}
+	return vec;
+}
+int main() {
+
+}
+// #include "pch.h"
+// #include "C:\Users\timof\source\repos\ConsoleApplication4\ConsoleApplication4.cpp"
+// #include <vector>
+// using namespace std;
+// vector<int> test_vec = { 3,4,5 };
+// TEST(TestCaseName, TestBool) {
+// 	ASSERT_EQ(test_vec, function({ 1,2,3 }, 2));
+// }
+// Как добавить в репозиторий тест я не понял, поэтому прикрепил как комментарий
